b2e69b6c7c6c5aef4f64ee07bde1458a01368277
josdafsos <wano90210@yandex.ru>
2025-01-01 22:53:43 +0200

    added multithreading, fixed memory leak with trajectories, minor refactoring, added text info

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import sys
 from simulation import Body
 from simulation import Simulation
 
+import time
+
 # Initialize Pygame
 pygame.init()
 
-# Set up the display
+# Set up the displachat
 width, height = 800, 800
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Solar system')
@@ -29,143 +31,102 @@ rot_vel_earth   = 30e3                            # Earth tangential velocity ar
 rot_vel_moon    = rot_vel_earth + 1.022e3         # Moons tangential velocity around Sun, m/s
 rot_vel_mars    = 24e3                            # Mars tangential velocity around Sun, m/s
 
-
-
 Simulation.init_static_params(width)
+Body.init_static_properties(width)
 simulation = Simulation(window=window)
 
 # Define instances of bodies
 bodies = [
     Body(name       = 'Sun', 
                color      = (252, 229, 112), 
-               radius     = 696e6/pix_to_m*10,         # radius adjusted by 10 for visualization 
+               radius     = 696e6*10,         # radius adjusted by 10 for visualization
                mass       = 2e30,                        
                pos_xy_vis = np.array([center_x_vis, center_y_vis]),
-               pos_xy     = np.array([max_distance,0]),                
-               dxdy       = np.array([0,0]),
-               d2xd2y     = np.array([0,0]),
-               d2xd2y_int = 0,
-               path_vis   = [(center_x_vis, center_y_vis)]),         
+               pos_xy     = np.array([max_distance, 0])),
     
     Body(name       = 'Venus', 
                color      = (255, 198, 73), 
-               radius     = 6.05e6/pix_to_m*500,        # radius adjusted by 500 for visualization 
+               radius     = 6.05e6*500,        # radius adjusted by 500 for visualization
                mass       = 4.57e24,                        
                pos_xy_vis = np.array([center_x_vis - 108.2e9/pix_to_m, height/2]),
-               pos_xy     = np.array([max_distance - 108.2e9,0]),                
-               dxdy       = np.array([0,0]),
-               d2xd2y     = np.array([0,0]),
-               d2xd2y_int = 0,
-               path_vis   = [(center_x_vis - 108.2e9/pix_to_m, height/2)]),
+               pos_xy     = np.array([max_distance - 108.2e9,0]),
+               vel = [0, rot_vel_venus]),
 
     Body(name       ='Mercury', 
                color      = (183, 184, 185), 
-               radius     = 2.44e6/pix_to_m*1000,       # radius adjusted by 500 for visualization
+               radius     = 2.44e6*1000,       # radius adjusted by 500 for visualization
                mass       = 3.3e23,
                pos_xy_vis = np.array([center_x_vis - 57.9e9/pix_to_m, height/2]),
-               pos_xy     = np.array([max_distance - 57.9e9,0]), 
-               dxdy       = np.array([0,0]),
-               d2xd2y     = np.array([0,0]),
-               d2xd2y_int = 0,
-               path_vis   = [(center_x_vis - 57.9e9/pix_to_m, height/2)]), 
+               pos_xy     = np.array([max_distance - 57.9e9,0]),
+               vel = [0, rot_vel_mercury]),
 
     Body(name       ='Earth', 
                color      = (0,94,184), 
-               radius     = 6371e3/pix_to_m*500,       # radius adjusted by 500 for visualization
+               radius     = 6371e3*500,       # radius adjusted by 500 for visualization
                mass       = 5.97e24,
                pos_xy_vis = np.array([center_x_vis - 149e9/pix_to_m, height/2]),
-               pos_xy     = np.array([max_distance - 149e9,0]), 
-               dxdy       = np.array([0,0]),
-               d2xd2y     = np.array([0,0]),
-               d2xd2y_int = 0,
-               path_vis   = [(center_x_vis - 149e9/pix_to_m, height/2)]),      
+               pos_xy     = np.array([max_distance - 149e9,0]),
+               vel = [0, rot_vel_earth]),
 
     Body(name       ='Moon', 
                color      = (246, 241, 213), 
-               radius     = 1737e3/pix_to_m*200,       # radius adjusted by 200 for visualization
+               radius     = 1737e3*200*2,       # radius adjusted by 200 for visualization
                mass       = 7.35e22,
                pos_xy_vis = np.array([center_x_vis - (149e9+384.4e6)/pix_to_m, height/2]),
-               pos_xy     = np.array([max_distance - (149e9+384.4e6),0]), 
-               dxdy       = np.array([0,0]),
-               d2xd2y     = np.array([0,0]),
-               d2xd2y_int = 0,
-               path_vis   = [(center_x_vis - (149e9+384.4e6)/pix_to_m, height/2)]),
+               pos_xy     = np.array([max_distance - (149e9+384.4e6),0]),
+               vel= [0, rot_vel_moon]),
 
     Body(name       ='Mars', 
                color      = (156, 46, 53), 
-               radius     = 3.4e6/pix_to_m*500,       # radius adjusted by 500 for visualization
+               radius     = 3.4e6*500,       # radius adjusted by 500 for visualization
                mass       = 6.42e23,
                pos_xy_vis = np.array([center_x_vis - (228e9)/pix_to_m, height/2]),
-               pos_xy     = np.array([max_distance - (228e9),0]), 
-               dxdy       = np.array([0,0]),
-               d2xd2y     = np.array([0,0]),
-               d2xd2y_int = 0,
-               path_vis   = [(center_x_vis - (228e9)/pix_to_m, height/2)]),             
-    
+               pos_xy     = np.array([max_distance - (228e9),0]),
+               vel= [0, rot_vel_mars]),
 ]
 
 simulation.add_body(bodies=bodies)
+simulation.add_info("days")
 
 # Initialize the clock
 clock = pygame.time.Clock()
-fps   = 60          # fps set to hours
-t     = 0           # current time step
+fps = 30          # fps set to hours
 
-# Main loop
-running = True
-button_pressed = None
-while running:
-    """ for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False """
 
-    # Clear the screen
-    window.fill((255, 255, 255))
+def main_loop():
+    button_pressed = None
+    running = True
 
-    # If space bar gets pressed --> rotation is initiated
-    # keys = pygame.key.get_pressed()
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYUP:
-            #print(f'Key released: {pygame.key.name(event.key)}')
-            if event.key == pygame.K_SPACE:
-                print("space is released")
-                if button_pressed is None:
-                    button_pressed = True
-                    bodies[0].dxdy = [0,0]
-                    bodies[1].dxdy = [0,rot_vel_venus]        
-                    bodies[2].dxdy = [0,rot_vel_mercury]        
-                    bodies[3].dxdy = [0,rot_vel_earth]
-                    bodies[4].dxdy = [0,rot_vel_moon]
-                    bodies[5].dxdy = [0,rot_vel_mars]
-                else:
-                    button_pressed = not button_pressed
-        
-        
+    simulation.run_in_thread()  # runs the game logic in a separate thread
+    # use simulation.logic_fps = 100 # to change computation frequency
+    simulation.logic_fps = 1000
 
-    # Draw the bodies
-    #for body in bodies:
-    #    body.draw(window)
+    while running:
+        # event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE: # If space bar gets pressed --> rotation is initiated
+                    print("space is released")
+                    if button_pressed is None:
+                        button_pressed = False
+                    else:
+                        button_pressed = not button_pressed
+                    simulation.is_paused = button_pressed
 
-        
-    if button_pressed: 
-        simulation.step()
-        print(t)
-        t = t + simulation.dt/60/60/24  # days
-                
+        # drawing section:
+        window.fill((0, 0, 0))  # Clear the screen
+        simulation.draw_all()
 
-    simulation.draw_all()
+        # Flip the display
+        pygame.display.flip()
 
-    # Flip the display
-    pygame.display.flip()
+        # Control the framerate
+        clock.tick(fps)
 
-    # Control the framerate 
-    clock.tick(fps)
 
-    
-
+main_loop()
 # Quit Pygame
 pygame.quit()
 sys.exit()
--- simulation.py
+++ simulation.py
@@ -1,22 +1,35 @@
 import pygame
 import numpy as np
-
+from collections import deque
+import time
+import threading
 
 # Define Body class
 class Body: 
 
+    radius_scaler = None
 
-    def __init__(self, name, color, radius, mass, pos_xy_vis, pos_xy, dxdy, d2xd2y, d2xd2y_int, path_vis):
-        self.name        = name
-        self.color       = color
-        self.radius      = radius           # radius in kilometers
-        self.mass        = mass             # mass in kg 
-        self.pos_xy_vis  = pos_xy_vis       # position in pixels 
-        self.pos_xy      = pos_xy           # position in meters
-        self.dxdy        = dxdy             # velocity in kilometers per second
-        self.d2xd2y      = d2xd2y           # acceleration in kilometers^2 per second  
-        self.d2xd2y_int  = d2xd2y_int       # placeholder to store results of the intermediate calculations
-        self.path_vis    = path_vis
+    @staticmethod
+    def init_static_properties(width):
+        max_distance = 228e9  # maximum distance (Mars to SUn) in meters
+        max_distance = 1.1 * max_distance  # maximum distance in meters
+        Body.radius_scaler = max_distance / (width / 2)  # conversion of pixels to km
+
+    def __init__(self, name, color, radius, mass, pos_xy_vis, pos_xy, vel=np.array([0, 0]) ):
+        self.name = name
+        self.color = color
+        self.radius = radius / Body.radius_scaler            # radius in kilometers
+        self.mass = mass             # mass in kg
+        self.pos_xy_vis = np.array(pos_xy_vis)       # position in pixels
+        self.pos_xy = np.array(pos_xy)           # position in meters
+        self.vel = np.array(vel)             # velocity in kilometers per second
+        self.acc = np.array([0, 0])           # acceleration in kilometers^2 per second
+        self.d2xd2y_int = 0       # placeholder to store results of the intermediate calculations
+        self.path_vis = deque(maxlen=1000)  # change maxlen to modify the trajectory tracking
+        self.path_vis.append(self.pos_xy_vis.copy())
+        self.path_vis.append(self.pos_xy_vis.copy())  # the vector must have at least to lines to be drawn correctly
+
+
         
 
     def draw(self, surface):
@@ -24,14 +37,14 @@ class Body:
 
 
     def update_position(self, pix_to_m, dt):
-        self.dxdy       = self.dxdy + self.d2xd2y*dt
-        self.pos_xy     = self.pos_xy + self.dxdy*dt
-        self.pos_xy_vis = self.pos_xy_vis + self.dxdy*dt/pix_to_m
+        self.vel       = self.vel + self.acc * dt
+        self.pos_xy     = self.pos_xy + self.vel * dt
+        self.pos_xy_vis = self.pos_xy_vis + self.vel * dt / pix_to_m
 
+    def track_trajectory(self):
+        self.path_vis.append((self.pos_xy_vis[0], self.pos_xy_vis[1]))
 
     def draw_path(self, surface):
-        self.path_vis.append((self.pos_xy_vis[0],self.pos_xy_vis[1])) 
-
         pygame.draw.lines(surface = surface, 
                           color = self.color,
                           closed = False, 
@@ -54,57 +67,120 @@ class Simulation:
 
     G_const = 6.67e-11                        # gravitational constant, N*m^2*kg^-2
     
-    def __init__(self, window=None, dt=60*60*24):
+    def __init__(self, window=None, dt=60*60*24, compute_alg="old", logic_fps=1000):
+        """
+        :param window: used to define in which window the drawing will occur.If window is None (default) the nothing will be drawn
+        :param compute_alg: "vec" or "old" sets the algorith to be used for physics computation. Old is initial alg and vec is the new one
+        :param logic_fps: frequency Hz, at which simulation logic will be computed
+        """
         self.bodies = []
         self.dt = dt  # dt is in seconds
         self.cur_time = 0
         self.window = window
+        if compute_alg == "vec":
+            self.compute_physics = self._vec_physics_compute
+        elif compute_alg == "old":
+            self.compute_physics = self._old_physics_compute
+        else:
+            raise Exception("Unknown physics engine type")
+
+        self.info = []  # list of tracking info to write
+        self.font = pygame.font.Font(None, 20)  # None for default font, 50 is the font size  font = pygame.font.SysFont('Arial', 50)
+
+        self.is_paused = True
+        self.is_running = True  # kills multithread if false (redesign the name)
+
+        # multithreading variables
+        self.logic_fps = logic_fps
+        self.current_time = time.time()
+        self.last_logic_update_time = time.time()
+
+    def run_in_thread(self):
+        simulatiom_thread = threading.Thread(target=self._thread_run)
+        simulatiom_thread.daemon = True  # Ensure the logic thread is killed when the program ends
+        simulatiom_thread.start()
+
+    def _thread_run(self):
+        while self.is_running:
+            self.current_time = time.time()
+            elapsed_time = self.current_time - self.last_logic_update_time
+            if elapsed_time >= (1 / self.logic_fps):  # Update game logic as fast as possible
+                self.step()
+                self.last_logic_update_time = self.current_time
 
     def add_body(self, body=None, bodies=None):
-        if not (body is None):
+        if body is not None:
             self.bodies.append(body)
-        elif not (bodies is None):
+        elif bodies is not None:
             self.bodies.extend(bodies)
         else:
             print("Error, body was not provided correctly")
 
+    def _vec_physics_compute(self):
+        pass
 
-    def step(self):
-        for ind_1 in range(0,len(self.bodies)): 
+    def _old_physics_compute(self):
+        for ind_1 in range(0, len(self.bodies)):
 
-            d2xd2y_data = np.array([0,0])
+            d2xd2y_data = np.array([0, 0])
 
-            for ind_2 in range(0,len(self.bodies)):
+            for ind_2 in range(0, len(self.bodies)):
 
-                if self.bodies[ind_1].name != self.bodies[ind_2].name: 
+                if self.bodies[ind_1].name != self.bodies[ind_2].name:
 
-                    pos_diff    = self.bodies[ind_1].pos_xy - self.bodies[ind_2].pos_xy                   # difference in positions, m
-                    distance    = np.sqrt(pos_diff[0]**2 + pos_diff[1]**2)                      # distance between objects, m
-                    force       = Simulation.G_const*self.bodies[ind_1].mass*self.bodies[ind_2].mass/(distance**2)   # gravitational force, N 
-                    
+                    pos_diff = self.bodies[ind_1].pos_xy - self.bodies[ind_2].pos_xy  # difference in positions, m
+                    distance = np.sqrt(pos_diff[0] ** 2 + pos_diff[1] ** 2)  # distance between objects, m
+                    force = Simulation.G_const * self.bodies[ind_1].mass * self.bodies[ind_2].mass / (
+                                distance ** 2)  # gravitational force, N
+
                     # adjusting the sign of the force
-                    if self.bodies[ind_1].mass > self.bodies[ind_2].mass: 
-                        d2dx          = force/self.bodies[ind_1].mass*pos_diff[0]/distance
-                        d2dy          = force/self.bodies[ind_1].mass*pos_diff[1]/distance    
+                    if self.bodies[ind_1].mass > self.bodies[ind_2].mass:
+                        d2dx = force / self.bodies[ind_1].mass * pos_diff[0] / distance
+                        d2dy = force / self.bodies[ind_1].mass * pos_diff[1] / distance
                     else:
-                        d2dx          = -1*force/self.bodies[ind_1].mass*pos_diff[0]/distance
-                        d2dy          = -1*force/self.bodies[ind_1].mass*pos_diff[1]/distance    
-                        
-                    d2xd2y        = np.array([d2dx,d2dy])                    
-                    d2xd2y_data   = d2xd2y_data + d2xd2y
+                        d2dx = -1 * force / self.bodies[ind_1].mass * pos_diff[0] / distance
+                        d2dy = -1 * force / self.bodies[ind_1].mass * pos_diff[1] / distance
+
+                    d2xd2y = np.array([d2dx, d2dy])
+                    d2xd2y_data = d2xd2y_data + d2xd2y
 
             self.bodies[ind_1].d2xd2y_int = d2xd2y_data.copy()
 
-        for ind_3 in range(0,len(self.bodies)):
-            
-            self.bodies[ind_3].d2xd2y = self.bodies[ind_3].d2xd2y_int
-            self.bodies[ind_3].update_position(Simulation.pix_to_m, dt=self.dt)    
+        for ind_3 in range(0, len(self.bodies)):
+            self.bodies[ind_3].acc = self.bodies[ind_3].d2xd2y_int
+            self.bodies[ind_3].update_position(Simulation.pix_to_m, dt=self.dt)
             self.bodies[ind_3].draw_path(self.window)
 
+    def step(self):
+        if self.is_paused:
+            return
+        self.compute_physics()
+        for body in self.bodies:
+            body.track_trajectory()
         self.cur_time += self.dt
 
+    def add_info(self, info: str):
+        """
+        adds a tracking of a value and puts a text information
+        :param info: type of value to be visualized
+        Supported info types: "days" - number of days in the simulation
+        """
+
+        self.info.append(info)
+
+    def _draw_info(self):
+        # TODO add energy texting option
+        for i in range(len(self.info)):
+            if self.info[i] == "days":
+                message = "Days passed: " + str(self.cur_time/60/60/24)
+                text = self.font.render(message, True, (255, 255, 255))  # White text
+            self.window.blit(text, (10, 20 + 30*i))
 
     def draw_all(self):
-        for body in self.bodies:
-            body.draw(self.window)
-            body.draw_path(self.window)
+        if self.window is not None:  # if it is none, the visualization is skipped for faster computations
+            for body in self.bodies:
+                body.draw(self.window)
+                body.draw_path(self.window)
+        self._draw_info()
+
+
